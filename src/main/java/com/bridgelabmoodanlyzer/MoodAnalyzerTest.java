0f1efa13faee4c1b76a608ca51bc08fc7c2cfba3
Ameet Jadhav <ammugs11@gmail.com>
2022-08-04 18:43:02 +0530

    added using constructer return sad

---
+++ src/main/java/com/bridgelabmoodanlyzer/MoodAnalyzerTest.java
@@ -0,0 +1,16 @@
+package com.bridgelabmoodanlyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+	
+
+	@Test
+    public void given_SadMood_Should_Return_Sad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzerMood("I am In a Sad Mood");
+        Assert.assertEquals("SAD", mood);
+}
+
+}
