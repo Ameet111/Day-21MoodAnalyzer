44efa8b76da2e110140c1fdf0daff71bd3a4ae98
Ameet Jadhav <ammugs11@gmail.com>
2022-08-04 19:18:52 +0530

    added handle exception invalid mood

---
+++ src/main/java/com/bridgelabhandleexcepation/MoodAnalyzer.java
@@ -0,0 +1,38 @@
+package com.bridgelabhandleexcepation;
+
+/**
+ * Purpose - Handle Exception if User Provides Invalid Mood
+ *
+ */
+
+public class MoodAnalyzer {
+	
+	private String message;
+
+	// Default Constructor
+	public MoodAnalyzer() {
+	}
+
+	// Parameterized constructor
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	// Handling Exception
+	public String analyseMood() {
+		try {
+
+			if (this.message.contains("Sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch (NullPointerException e) {
+			return "HAPPY";
+		}
+	}
+	
+	
+	
+	
+
+}
